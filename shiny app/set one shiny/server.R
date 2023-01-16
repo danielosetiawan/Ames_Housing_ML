7b68456185b62379903eae01fbcbae7525939a42
set-one <danielosetiawan@gmail.com>
2023-01-16 13:35:02 -0800

    updates

--- shiny app/set one shiny/server.R
+++ shiny app/set one shiny/server.R
@@ -3,9 +3,6 @@
 
 function(input, output, session) {
   
-  output$timeseries = renderPlot(
-    expr = all_neighborhoods()
-  )
   
   # assignments if neighborhood changes
   
@@ -22,79 +19,194 @@ function(input, output, session) {
     
     
     ##### budget slider
-    min_price = min(df_neighborhood()$SalePrice)
-    max_price = max(df_neighborhood()$SalePrice)
-    mid_price = max_price - min_price
-    first_qtl = signif(min_price + 0.25 * mid_price, 2)
-    third_qtl = signif(max_price - 0.25 * mid_price, 2)
+    # min_price = min(df_neighborhood()$SalePrice)
+    # max_price = max(df_neighborhood()$SalePrice)
+    # mid_price = max_price - min_price
+    # first_qtl = signif(min_price + 0.25 * mid_price, 2)
+    # third_qtl = signif(max_price - 0.25 * mid_price, 2)
     
-    updateSliderInput(
-      session,
-      inputId = 'budget',
-      min = signif(min_price, 2),
-      max = signif(max_price, 2),
-      value = c(first_qtl, third_qtl)
-    )
-    
-    # picker labels
+    # updateSliderInput(
+    #   session,
+    #   inputId = 'budget',
+    #   min = signif(min_price, 2),
+    #   max = signif(max_price, 2),
+    #   value = c(first_qtl, third_qtl)
+    # )
   
+
+### timeseries
+    if (input$neighborhood == 'All Neighborhoods') {
+      sarima_model = './img/Ames_sarima_prediction.png'
+    } else {
+      sarima_model = paste0('./img/', input$neighborhood, 
+                            '_sarima_prediction.png')
+    }
+  
+    output$sarima = renderUI({
+      HTML(paste0('<img width = "100%", height = "70%",
+                    src= "', sarima_model, '"/>'))
+    })
     
+### addresses
     updatePickerInput(
       session,
-      inputId = 'undervalued',
-      # label = c(slider_min, slider_max),
-      selected = NULL,
-      choices = undervalued()$Prop_Addr,
+      inputId = 'address',
+      # label = label,
+      # selected = NULL,
+      choices = df_undervalued()$Prop_Addr,
       choicesOpt = list(
         subtext = paste0(
           c('\t\t'), 'Price: $', 
-          undervalued()$SalePrice, ' (',
-          undervalued()$Value,
-          undervalued()$Delta, ')'
+          df_undervalued()$SalePrice, ' (',
+          df_undervalued()$Value,
+          df_undervalued()$Delta, ')'
           )
-        ),
-      options = pickerOptions(virtualScroll = TRUE,
-                              dropupAuto = FALSE,
-                              width = '70%',
-                              title = FALSE)
+        )
     )
+    
+    output$undervalued_dt = renderDataTable(
+      server = FALSE,
+      datatable(df_undervalued(), 
+      filter = 'top', 
+      selection = 'single',
+      options = list(
+        pageLength = 5, 
+        scrollY = "600px", 
+        autoWidth = TRUE))
+    )
+    
   })
+  
+  observeEvent(input$address, {
+    
+    ## info boxes
+    output$crime_rate <- renderValueBox({
+      valueBox(
+        value = tags$p('Crime Rate', style = "font-size: 50%;"),
+        subtitle=tags$p(paste0(df_property()$crime_rate, ' / 10'), 
+                        style = "font-size: 200%;"),
+        icon = icon('handcuffs'),
+        color = 'purple')
+    })
+    
+    
+    output$school_quality <- renderValueBox({
+      valueBox(
+        value = tags$p('School Quality', style = "font-size: 50%;"),
+        subtitle=tags$p(paste0(df_property()$school_quality, ' / 10'), 
+                        style = "font-size: 200%;"),
+        icon = icon('graduation-cap'),
+        color = 'light-blue')
+    })
+    
+    output$income <- renderValueBox({
+      
+      valueBox(
+        value = tags$p('Neighborhood Income', style = "font-size: 50%;"),
+        subtitle=tags$p(paste0(df_property()$nb_income, ' / 10'), 
+                        style = "font-size: 200%;"),
+        icon = icon('sack-dollar'),
+        color = 'olive')
+    })
+    
+    output$appreciation <- renderValueBox({
+      
+      valueBox(
+        value = tags$p('Neighborhood Appreciation', style = "font-size: 50%;"),
+        subtitle=tags$p(paste0(df_property()$nb_appreciation, ' / 10'), 
+                        style = "font-size: 200%;"),
+        icon = icon('arrow-trend-up'),
+        color = 'yellow')
+    })
+    
+    output$saleprice <- renderValueBox({
+      
+      price = paste0(round(df_property()$SalePrice / 1000), 'K')
+      
+      valueBox(
+        value = tags$p('Sale Price', style = "font-size: 50%;"),
+        subtitle=tags$p(price, style = "font-size: 200%;"),
+        icon = icon('sack-dollar'),
+        color = 'light-blue')
+    })
+    
+    
+    ## current home
+    
+    output$current_home <- renderUI({
+      
+      sale_price = paste0(round(df_property()$SalePrice / 1000), 'K')
+      predicted_price = paste0(round(df_property()$Predicted / 1000), 'K')
+      accommodations = paste0(
+        df_property()$BedroomAbvGr, 'BR / ', df_property()$FullBath + 
+          df_property()$HalfBath / 2, 'BA')
+      
+      
+      if (df_property()$Delta/1e3 <= -5) {
+        value = 'UNDERVALUED'
+        color = 'green'
+      } else if (df_property()$Delta/1e3 >= 5) {
+        value = 'OVERVALUED'
+        color = 'red'
+      } else {
+        value = 'FAIR PRICE'
+        color = 'orange'
+      }
+      
+      HTML(
+        paste0('<b><span style="color:', color, '">', value, '</span></b><br><br>',
+               'Sale Price: <b>', sale_price, '</b><br>',
+               'Predicted Price: <b>', predicted_price, '</b><br>',
+               'Accommodations: <b>', accommodations, '</b><br>',
+               'Year Built: <b>', df_property()$YearBuilt, '</b><br>',
+               'Year Remodeled: <b>', df_property()$YearRemodAdd, '</b><br>',
+               'Total SF: <b>', df_property()$GrLivArea, '</b><br>',
+               '1st floor SF: <b>', df_property()$X1stFlrSF, '</b><br>',
+               '2nd floor SF: <b>', df_property()$X2ndFlrSF, '</b><br>',
+               'Total Basement SF: <b>', df_property()$TotalBsmtSF, '</b><br>',
+               'Overall Quality: <b>', df_property()$OverallQual, '/10</b><br>',
+               'Overall Condition: <b>', df_property()$OverallCond, '/10</b><br>',
+               'Kitchen Quality: <b>', df_property()$KitchenQual, '/5</b><br>',
+               'Garage Quality: <b>', df_property()$GarageQual, '/5</b><br>',
+               'Basement Quality: <b>', df_property()$BsmtQual, '/5</b><br>'
+           ))
+      
+    })
+    
+  })
+  
+  
   ############ reactive functions ############
     
-  # budget slider
     df_neighborhood = reactive({
+
       if (input$neighborhood == 'All Neighborhoods') {
-        df
+        df_predictions
       } else {
-        df %>%
+        df_predictions %>%
           filter(Neighborhood == input$neighborhood)
       }})
     
-    # dropdown predictions
-    predictions = reactive({
-      if (input$neighborhood == 'All Neighborhoods') {
-        df_predictions %>%
-          # filter(input$budget[2] < 2e5 | input$budget[1] > 5.7e5) %>%
-          head(25)
-      } else {
-        picker_label = df_predictions %>%
-          filter(Neighborhood == input$neighborhood,
-                 input$budget[1] < 2e5 | input$budget[2] > 5.7e5) %>%
-          head(25)
-      }})
-    
-    undervalued = reactive({
-      predictions() %>%
-      select(Prop_Addr, Delta, SalePrice) %>%
-      mutate(SalePrice = paste0(round(as.numeric(SalePrice / 1000)), 'K'),
-             Value = case_when(as.numeric(Delta/1e3) <= -5  ~ 'undervalued by ',
-                               as.numeric(Delta/1e3) >= 5 ~ 'overvalued by ',
-                               TRUE ~ 'fair price'),
-             Delta = ifelse(Value == 'fair price', '', 
-                            paste0(abs(round(as.numeric(Delta/1e3))), 'K')))
+    df_property = reactive({
+      df_predictions %>%
+        filter(Prop_Addr == input$address)
     })
     
-
+    
+    
+    df_undervalued = reactive({
+      df_neighborhood() %>%
+        head(25) %>%
+        select(Prop_Addr, Delta, SalePrice) %>%
+        mutate(SalePrice = paste0(round(as.numeric(SalePrice / 1000)), 'K'),
+               Value = case_when(as.numeric(Delta/1e3) <= -5  ~ 'undervalued by ',
+                                 as.numeric(Delta/1e3) >= 5 ~ 'overvalued by ',
+                                 TRUE ~ 'fair price'),
+               Delta = ifelse(Value == 'fair price', '', 
+                              paste0(abs(round(as.numeric(Delta/1e3))), 'K')))
+    })
+    
+    ####################
   
   
 }
--- shiny app/set one shiny/ui.R
+++ shiny app/set one shiny/ui.R
@@ -2,10 +2,13 @@ library(shiny)
 library(shinydashboard)
 
 # # Define UI for application
+spinner = 'folding-cube'
 
 dashboardPage(
-  dashboardHeader(title = "Basic dashboard"),
+  dashboardHeader(
+    title = ''),
   dashboardSidebar(
+    collapsed = TRUE,
     sidebarMenu(
       menuItem(
         "Homepage", 
@@ -24,41 +27,146 @@ dashboardPage(
     tabItems(
       tabItem(
         tabName = "dreamhome",
+        fluidPage(
+          theme = 'journal',
+          fluidRow(
+            box(
+              title=tagList('Location', icon('location-dot')),
+              status='success',
+              collapsible=TRUE,
+              width = 4,
+              height = 200, 
+                selectInput(
+                  inputId = 'neighborhood', 
+                  label = 'Neighborhood',
+                  choices = c('All Neighborhoods', 
+                              unique(df_predictions$Neighborhood)),
+                  selected = 'Neighborhood'
+                ),
+                pickerInput(
+                  inputId = 'address',
+                  label = 'Homes',
+                  choices = df_predictions$Prop_Addr),
+                  options = pickerOptions(
+                    virtualScroll = TRUE,
+                    dropupAuto = FALSE,
+                    width = '70%',
+                    title = FALSE
+            )),
+          
+            column(
+              width = 4,
+              style='padding:3px',
+                valueBoxOutput(width=NULL, 'crime_rate'),
+                valueBoxOutput(width=NULL, 'school_quality')),
+          column(
+            width = 4, 
+            style='padding:3px',
+                valueBoxOutput(width=NULL, 'income'),
+                valueBoxOutput(width=NULL, 'appreciation'),
+              
+          )),
         
-          selectInput(
-          inputId = 'neighborhood', 
-          label = 'Neighborhood',
-          choices = c('All Neighborhoods', unique(df$Neighborhood)),
-          selected = 'Neighborhood',
-          width = '28%'),
-        sliderInput(inputId = 'budget', 
-                    label = 'Budget',
-                    min = min(df$SalePrice), 
-                    max = max(df$SalePrice),
-                    value = c(2e5, 5e5)),
-        tabsetPanel(type = "tabs",
-                    tabPanel(
-                      'Homes', 
-                      pickerInput(
-                        inputId = 'undervalued',
-                        label = 'Browse all undervalued homes...', 
-                        choices = unique(undervalued25$Prop_Addr),
-                        choicesOpt = list(
-                          subtext = paste0(' Price: $',
-                            undervalued25$SalePrice, ' (',
-                            undervalued25$Value, '$',
-                            undervalued25$Delta, ')')),
-                        options = pickerOptions(virtualScroll = TRUE,
-                                                dropupAuto = FALSE,
-                                                width = '70%')
-                      )
-                    ),
-                    tabPanel(
-                      'Time Prediction', verbatimTextOutput("summary")
-                    ),
-                    tabPanel(
-                      '', tableOutput("table")
-                    )
+##################################################################
+
+          verticalTabsetPanel(
+            menuSide = 'right',
+            
+            verticalTabPanel(
+              title = 'Time Forecasting', box_height = 5,
+              icon = icon("line-chart", class = "fa-2x"),
+              # tags$b('Forecasted Time Analysis of Homes in Ames'),
+              tags$br(), tags$br(), tags$br(), 
+              addSpinner(
+                uiOutput(outputId = "sarima", height = "100%"),
+                spin = spinner
+              )
+            ),
+            
+            verticalTabPanel(
+              title = 'Parameter Tuning', box_height = 5,
+              icon = icon('gears', class = "fa-2x"),
+              
+              fluidRow(
+                column(
+                  width = 5,
+                  offset = 1,
+                  style='padding:3px',
+                  box(
+                    width = NULL,
+                    title=tagList('Current Home', icon('house')),
+                    status = 'info', 
+                    solidHeader = TRUE,                    
+                    collapsible = TRUE,
+                    uiOutput(outputId = 'current_home')
+                  )),
+                column(
+                    width = 5,
+                    style='padding:3px',
+                    box(
+                      width = NULL,
+                      title=tagList('What if...', icon('question')),
+                      status = 'info', 
+                      solidHeader = TRUE,                    
+                      collapsible = TRUE,
+                      sliderInput(inputId = "animation", 
+                                  label = "Looping Animation:",
+                                  min = 1, max = 2000,
+                                  value = 1, step = 10,
+                                  animate =
+                                    animationOptions(interval = 300, loop = TRUE)),
+                      valueBoxOutput(width=NULL, 'prediction')),
+                  
+                  
+                
+                )
+              )
+            ),
+            
+            verticalTabPanel(
+              box_height = 5,
+              title = "Exchange", icon = icon("exchange", class = "fa-2x"),
+              addSpinner(
+                billboarderOutput(outputId = "plot_exchange", height = "100%"),
+                spin = spinner
+              ),
+              materialSwitch(
+                inputId = "by_country", 
+                label = "See details by country", 
+                value = FALSE, right = TRUE
+              )
+            ),
+            
+            verticalTabPanel(
+              box_height = 5,
+              title = "Generation capacities", icon = icon("industry", class = "fa-2x"),
+              radioGroupButtons(
+                inputId = "capacities_plot", 
+                label = "Active production units", 
+                choiceNames = list(
+                  tags$span(icon("percent"), "Summary"),
+                  tags$span(icon("map"), "Map"), 
+                  tags$span(icon("industry"), "Global")
+                ),
+                choiceValues = c("summary", "map", "global"),
+                status = "dreamrs", justified = TRUE, selected = "summary"
+              ),
+              conditionalPanel(
+                condition = "input.capacities_plot == 'summary'",
+                addSpinner(
+                  billboarderOutput(outputId = "plot_active_units_p", height = "100%"),
+                  spin = spinner
+                )
+              ),
+              conditionalPanel(
+                condition = "input.capacities_plot != 'summary'",
+                addSpinner(
+                  leafletOutput(outputId = "map_capacities", height = "100%"),
+                  spin = spinner
+                )
+              )
+            )
+          )
         )
       )
     )
